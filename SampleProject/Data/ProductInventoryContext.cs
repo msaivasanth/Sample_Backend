c5700266f5d24929b85b501b8abce8a0d8d14ad6
msaivasanth <vasanths314@gmail.com>
2024-05-28 23:02:52 +0530

    Created a login api.

---
+++ SampleProject/Data/ProductInventoryContext.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SampleProject.Models.ProductInventory;
+
+namespace SampleProject.Data;
+
+public partial class ProductInventoryContext : DbContext
+{
+    public ProductInventoryContext()
+    {
+    }
+
+    public ProductInventoryContext(DbContextOptions<ProductInventoryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Image> Images { get; set; }
+
+    public virtual DbSet<Login> Logins { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=VASANTH\\SQLEXPRESS;Initial Catalog=Product_Inventory;Integrated Security=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.BrandId).HasName("PK__Brands__AAB3216F05B584DB");
+
+            entity.Property(e => e.BrandId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Brand_ID");
+            entity.Property(e => e.BrandName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Brand_Name");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__6DB38D4ED2095DF0");
+
+            entity.Property(e => e.CategoryId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Category_ID");
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Category_Name");
+        });
+
+        modelBuilder.Entity<Image>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .HasColumnName("Image_URL");
+
+            entity.HasOne(d => d.IdNavigation).WithMany()
+                .HasForeignKey(d => d.Id)
+                .HasConstraintName("FK__Images__ID__5CD6CB2B");
+        });
+
+        modelBuilder.Entity<Login>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Login");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Password)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.UserName)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("User_Name");
+
+            entity.HasOne(d => d.IdNavigation).WithMany()
+                .HasForeignKey(d => d.Id)
+                .HasConstraintName("FK__Login__ID__5AEE82B9");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC27B5F7FD82");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.BrandId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Brand_Id");
+            entity.Property(e => e.CategoryId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Category_ID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(1000)
+                .IsUnicode(false);
+            entity.Property(e => e.Thumbnail)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.Title)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Products)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("FK__Products__Thumbn__5441852A");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Products__Catego__5535A963");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC271F7AF360");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.ProfileImage)
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .HasColumnName("Profile_Image");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SampleProject.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +13,35 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+
+builder.Services.AddDbContext<ProductInventoryContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        };
+    });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -23,6 +51,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("corspolicy");
+
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
